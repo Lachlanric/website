653e0dec0f5b035871d4d7493ef6d3f263d53c85
Lachlan Richards <lachlanric@outlook.com>
2023-11-12 00:48:00 +1100

    Adjust screen and img-tabs for mobile

--- js/modules.js
+++ js/modules.js
@@ -1,16 +1,22 @@
 function adjustForMobile() {
     if (screen.width <= 699) {
-        [...document.querySelectorAll('.side-spacer').forEach(e=>e.remove())];
-        [...document.querySelectorAll('.spacer-container').forEach(e=>{
+        [...document.querySelectorAll('.side-spacer')].forEach(e=>e.remove());
+        [...document.querySelectorAll('.spacer-container')].forEach(e=>{
             e.style.display = 'block';
             e.gridTemplateColumns = 'none';
-        })];
+        });
+
+        [...document.querySelectorAll('img-tabs')].forEach(e=>{
+            const contentWidth = [...e.childNodes[0].childNodes].reduce((a,div)=>(a + Number(div.clientWidth)), 0);
+            e.style.transformOrigin = "left top";
+            e.style.scale = (screen.width / contentWidth)*0.88;
+            console.log("contentWidth: "+contentWidth);
+            console.log("screen.width: "+screen.width);
+        });
 
     }
 
-    alert("screen.width: "+screen.width);
 }
 
 
 addEventListener("DOMContentLoaded", adjustForMobile);
-
